362c543bd4a03c74da53b13bf5a445a48faa7acc
jxhnkndl <jkroyston@gmail.com>
2024-05-30 18:55:56 -0400

    add single demo HTML route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Import Express
 const express = require('express');
 
+// Import Node path module
+const path = require('path');
+
 // Set dynamic server port
 const PORT = process.env.PORT || 3000;
 
@@ -14,20 +17,20 @@ app.use(express.urlencoded({ extended: true }));
 // Serving up the public directory to the browser
 app.use(express.static('public'));
 
+// Serve single HTML file when user visit /services endpoint
+app.get('/services', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/services.html'))
+);
+
 // CREATE -> http POST
 // READ ---> http GET
 // UPDATE -> http PUT
 // DELETE -> http DELETE
 
-const shows = [
-  "Mr. Robot",
-  "GOT",
-  "Fringe",
-  "3 Body Problem"
-];
+const shows = ['Mr. Robot', 'GOT', 'Fringe', '3 Body Problem'];
 
 // GET ALL SHOWS
-app.get("/api/shows", (req, res) => {
+app.get('/api/shows', (req, res) => {
   console.info(`GET /api/shows`);
 
   // Send back all shows
@@ -35,7 +38,7 @@ app.get("/api/shows", (req, res) => {
 });
 
 // GET SINGLE SHOW by ID (index in the shows array)
-app.get("/api/shows/:id", (req, res) => {
+app.get('/api/shows/:id', (req, res) => {
   console.info(`GET /api/shows/${req.params.id}`);
 
   // Extract show id (show's index position in shows array)
@@ -43,16 +46,16 @@ app.get("/api/shows/:id", (req, res) => {
 
   // If no matching show is found, respond 404 and return out of function
   if (!shows[id]) {
-    res.status(404).json({ msg: 'Show not found'});
+    res.status(404).json({ msg: 'Show not found' });
     return;
   }
 
-  // Send back the single show 
+  // Send back the single show
   res.status(200).json(shows[id]);
 });
 
 // ADD NEW SHOW
-app.post("/api/shows", (req, res) => {
+app.post('/api/shows', (req, res) => {
   console.info(`POST /api/shows`);
 
   // Extract the new show from the req.body
@@ -67,7 +70,7 @@ app.post("/api/shows", (req, res) => {
 });
 
 // UPDATE EXISTING SHOW BY ID
-app.put("/api/shows/:id", (req, res) => {
+app.put('/api/shows/:id', (req, res) => {
   // Capture show id from req.params
   const id = req.params.id;
 
@@ -76,7 +79,7 @@ app.put("/api/shows/:id", (req, res) => {
 
   // If no matching show is found, respond 404 and return out of function
   if (!shows[id]) {
-    res.status(404).json({ msg: 'Show not found'});
+    res.status(404).json({ msg: 'Show not found' });
     return;
   }
 
@@ -88,7 +91,7 @@ app.put("/api/shows/:id", (req, res) => {
 });
 
 // DELETE SHOW BY ID
-app.delete("/api/shows/:id", (req, res) => {
+app.delete('/api/shows/:id', (req, res) => {
   // Capture show id from req.params
   const id = req.params.id;
 
